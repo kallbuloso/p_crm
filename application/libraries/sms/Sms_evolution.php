fee36c35f88159e3f6df39b1ca5156cb52541152
kallbuloso <kallbuloso@gmail.com>
2024-12-01 14:54:46 -0300

    feat: Add SMS Evolution gateway for WhatsApp notifications
    This commit adds the `Sms_evolution` class to the `sms` library, allowing the application to send notifications via WhatsApp using the Evolution API. The `Sms_evolution` class extends the `App_sms` class and includes the necessary properties and methods for sending messages.
    
    The `add_gateway` method is called within the constructor to register the Evolution API as a gateway option. The gateway information includes a description, name, and options for the API URL, instance name, and API key.
    
    The `send` method is implemented to send a text message using the Evolution API. It constructs the API URL based on the configured API URL and instance name, and sends a POST request with the provided phone number and message. The response is then parsed and returned.
    
    This commit also includes the necessary imports and class definition for the `Sms_evolution` class.
    
    Recent user commits:
    - feat: Add sms_evolution gateway to app_init_sms_gateways function
    - add saas module
    - chore: Add .gitignore file to exclude /install/ directory
    - init 1

---
+++ application/libraries/sms/Sms_evolution.php
@@ -0,0 +1,74 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sms_evolution extends App_sms
+{
+    private $api_key;
+    private $api_url;
+    private $api_instance;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->api_key = $this->get_option('evolution', 'api_key');
+        $this->api_url = $this->get_option('evolution', 'api_url');
+        $this->api_instance = $this->get_option('evolution', 'api_instance');
+
+        $this->add_gateway('evolution', [
+            'info'  => '<p>Faz o envio de notificações via WhatsApp usando a API Evolution. </br></br>
+                            Caso não possua a API entre em contato comigo, te forneço baratinho. kkkkkkkkk
+                            <a href="https://wa.me/5511988291097" 
+                            target="_blank">Clique Aqui.</a></p><hr class="hr-10">
+                        </p>',
+            'name'    => 'Evolution Api Whatspp',
+            'options' => [
+                [
+                    'name'  => 'api_url',
+                    'label' => 'API URL',
+                    'info'  => '<p>Insira a URL de sua API. Exemplo: http://evolutionapi.com.br</p>'
+                ],
+                [
+                    'name'  => 'api_instance',
+                    'label' => 'Nome da instância',
+                    'info'  => '<p>Insira o nome da sua instância.</p>'
+                ],
+                [
+                    'name'  => 'api_key',
+                    'label' => 'API Key',
+                    'info'  => '<p>Insira a chave de API da sua instância.</p>'
+                ],
+            ],
+        ]);
+    }
+
+    public function send($number, $message)
+    {
+        $url = $this->api_url . "/message/sendText/" . $this->api_instance;
+
+        $response = $this->client->request('POST', $url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'apikey' => $this->api_key,
+            ],
+            'json' => [
+                'number' => $number,
+                'options' => [
+                    'delay' => 1200,
+                    'presence' => 'composing',
+                    'linkPreview' => false,
+                ],
+                'textMessage' => [
+                    'text' => $message,
+                ],
+            ],
+        ]);
+
+        $result = json_decode($response->getBody());
+
+        $error  = false;
+
+        return false;
+    }
+}
